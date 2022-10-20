c93eb887b465cce7214f7af99a8f83be3a8d6fd8
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2022-10-20 22:40:34 +0300

    toCardPlaceholder

--- src/components/screens/Home/transferMoney/TransferModal.tsx
+++ src/components/screens/Home/transferMoney/TransferModal.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {
-    Button, FormControl, FormErrorMessage, FormLabel, Input,
+    Button, FormControl, FormErrorMessage, FormLabel, Input, InputGroup, InputLeftElement, InputRightElement,
     Modal,
     ModalBody,
     ModalCloseButton,
@@ -11,8 +11,9 @@ import {
 } from "@chakra-ui/react";
 import {user} from "../Home";
 import {formatCardNumber} from "../../../../utls/formatCardNumber";
-import {SubmitHandler, useForm} from "react-hook-form";
+import {SubmitHandler, useForm, Controller} from "react-hook-form";
 import {ITransferData} from "./transferInterface";
+import {CheckIcon} from "@chakra-ui/icons";
 
 
 interface ITransferModal {
@@ -20,17 +21,20 @@ interface ITransferModal {
     onClose: () => void
 }
 
-
 const TransferModal: FC<ITransferModal> = ({isOpen, onClose}) => {
-
-
     const {
         handleSubmit,
         register,
-        formState: {errors, isSubmitting},
-    } = useForm<ITransferData>({mode: 'onChange'})
+        control,
 
-    const onSubmit: SubmitHandler<ITransferData> = data => { }
+        formState: {errors, isSubmitting},
+    } = useForm<ITransferData>({mode: 'onChange', defaultValues: {
+        amount: 0,
+            card: ''
+        }})
+
+    const onSubmit: SubmitHandler<ITransferData> = data => {
+    }
 
 
     return (
@@ -44,21 +48,41 @@ const TransferModal: FC<ITransferModal> = ({isOpen, onClose}) => {
                         <Stack>
                             <Input placeholder='From card' size='md' defaultValue={formatCardNumber(user.cardNumber)}
                                    disabled/>
+                            <Controller
+                                control={control}
+                                name='card'
+                                render={({field: {onChange, name, value}}) => (
+                                    <FormControl isInvalid={!!errors.card?.message}>
+                                        <Input
+                                            id={name}
+                                            size='md'
+                                            placeholder='To card'
+                                            value={formatCardNumber(value)}
+                                            onChange={e => onChange(e.target.value)}
+                                        />
+                                        <FormErrorMessage> {errors.card?.message} </FormErrorMessage>
+                                    </FormControl>
+                                )}
+                                rules={{
+                                    required: 'This is required',
+                                    minLength: {
+                                        value: 16, message: 'Minimum length should be 16'
+                                    }
+                                }}
+                            />
 
-                            <FormControl isInvalid={!!errors.card?.message}>
-                                <Input
-                                    id='name'
-                                    size='md'
-                                    placeholder='To card'
-                                    {...register('card', {
-                                        required: 'This is required',
-                                        minLength: {value: 16, message: 'Minimum length should be 16'},
-                                    })}
+                            <InputGroup>
+                                <InputLeftElement
+                                    pointerEvents='none'
+                                    color='gray'
+                                    fontSize=''
+                                    children='$'
+
                                 />
-                                <FormErrorMessage>
-                                    {errors.card?.message}
-                                </FormErrorMessage>
-                            </FormControl>
+                                <Input placeholder='Enter amount'    {...register('amount', {
+                                    required: 'This is required',
+                                })} />
+                            </InputGroup>
                             <Button variant='outline' mr='3'>Send money</Button>
                         </Stack>
 
