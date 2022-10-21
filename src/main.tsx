a5d8bb3a31f0cf661d36fe09ef7fe9e4a1adbbf9
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2022-10-21 12:54:14 +0300

    axios

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import './index.css'
 import Home from "./components/screens/Home/Home";
 import {ChakraProvider, extendTheme, ThemeConfig} from "@chakra-ui/react";
 import Theme from "./components/providers/Theme";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false
+        }
+
+    }
+});
 
 
 
@@ -11,8 +22,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
             <Theme>
+                <QueryClientProvider client={queryClient}>
 
             <Home/>
+                </QueryClientProvider>
             </Theme>
         </ChakraProvider>
     </React.StrictMode>
