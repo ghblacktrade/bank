16c6c73f066347d518302f8cddd6ca061534a8a4
Arseniy <97445616+ghblacktrade@users.noreply.github.com>
2022-10-22 12:02:52 +0300

    useProfile

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,8 +1,9 @@
-import {useQuery} from "@chakra-ui/react";
-import {UserService} from "../services/userService";
+
+import { useQuery } from '@tanstack/react-query'
+import { UserService } from '../services/user.service'
 
 export const useProfile = () => {
-
-  const {} =  useQuery(['profile'], () => UserService.getProfile())
+  const { data } = useQuery(['profile'], () => UserService.getProfile())
+  return { user: data }
 }
 
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,34 +1,42 @@
-import {instance} from "../api";
-import {IUser} from "../types/userInterface";
+import { instance } from '../api'
+import { IUser } from '../types/userInterface'
+
+export interface ITransferMoney {
+    amount: number
+    card: number
+    fromCard: number
+}
 
 export const UserService = {
     async getProfile() {
         const response = await instance.get<IUser>(`/users/1`)
         return response.data
     },
+
     async getUsers() {
-        const response = await instance.get< IUser > (`/users`)
+        const response = await instance.get<IUser[]>(`/users`)
         return response.data.filter(user => user.id !== 1)
     },
-    async getUserByCard(card: number){
-        const response = await instance.get<IUser[]>('/users',{
+
+    async getUserByCard(card: number) {
+        const response = await instance.get<IUser[]>('/users', {
             params: {
                 card
             }
         })
-
         return response.data[0]
     },
-    async transferMoney(amount : number, card: number, fromCard: number) {
+
+    async transferMoney({ amount, card, fromCard }: ITransferMoney) {
         const userFrom = await this.getUserByCard(fromCard)
         const userTo = await this.getUserByCard(card)
 
-        await instance.patch(`/users/${userFrom.id}`, {
-            amount: userFrom.amount - amount
-        })
-
-        await instance.patch(`/users/${userTo.id}`, {
-            amount : userTo.amount + amount
-        })
+        // await instance.patch(`/users/${userFrom.id}`, {
+        //     balance: userFrom.balance - amount
+        // })
+        //
+        // await instance.patch(`/users/${userTo.id}`, {
+        //     balance: userTo.balance + amount
+        // })
     }
 }
